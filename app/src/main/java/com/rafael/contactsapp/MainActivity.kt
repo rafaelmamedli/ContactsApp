3eb2a2217ec658543909efde8d3bd85b6fe0b224
rafae <rafael.mamedli.01@gmail.com>
2023-06-11 20:41:52 +0400

    Implementations added

---
+++ app/src/main/java/com/rafael/contactsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rafael.contactsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
